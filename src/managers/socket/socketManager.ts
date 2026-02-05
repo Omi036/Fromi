ea5803c34a429bebb3b9750b8c6120c03350f7e6
Omi036 <omible@protonmail.com>
2026-02-05 18:48:14 +0100

    Updated Socket Manager

--- src/managers/socket/socketManager.ts
+++ src/managers/socket/socketManager.ts
@@ -1,6 +1,43 @@
-import { createServer } from "http";
 import { Server as HTTPServer } from "http";
-import { Server as IOServer, Socket } from "socket.io";
+import { DefaultEventsMap, ExtendedError, Server as IOServer, Socket } from "socket.io";
+
+type MiddlewareFunction = (
+  socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
+  next: (err?: ExtendedError) => void
+) => void;
+
+
+class SocketChannel {
+    constructor(channel, fallback: (socket: Socket, ...args) => void, connectOnJoin = true){
+        this.channel = channel
+        this.fallback = fallback
+        this.connectOnJoin = true
+
+        SocketManager.addChannel(this)
+    }
+
+    // Alias
+    static new(channel, fallback: (socket: Socket, ...args) => void, connectOnJoin = true){
+        return new SocketChannel(channel, fallback, connectOnJoin)
+    }
+
+    appendToSocket(socket: Socket){
+        socket.on(this.channel, (...args) => this.fallback(socket, ...args))
+    }
+
+    channel: string
+    connectOnJoin: boolean
+    fallback: (socket: Socket, ...args) => void
+}
+
+class SocketMiddleware {
+    constructor(handleFunction: MiddlewareFunction){
+        this.handle = handleFunction
+        SocketManager.use(this)
+    }
+
+    handle: MiddlewareFunction
+}
 
 
 class SocketManager {
@@ -9,6 +46,9 @@ class SocketManager {
     private static _hasStarted: boolean
     private static _httpServer: HTTPServer
     private static _ioServer: IOServer
+
+    private static _middlewares: Array<SocketMiddleware> = []
+    private static _channels: Array<SocketChannel> = []
 
     static Handler = {
         type: "postembedded",
@@ -22,14 +62,40 @@ class SocketManager {
         if(this._hasStarted) return
         this._hasStarted = true
         this._ioServer = new IOServer(this._httpServer, {});
+
+        this._ioServer.on("connection", (socket: Socket) => {
+            for(const channel of this._channels){
+                if(!channel.connectOnJoin) return
+                socket.on(channel.channel, (...args) => channel.fallback(socket, ...args))
+            }
+            this.onClientConnection(socket);
+        })
+
+        for(const middleware of this._middlewares){
+            this._ioServer.use(middleware.handle)
+        }
     }
 
-    static async emit(channel: string, message: any){
+    static use(middleware: SocketMiddleware){
+        this._middlewares.push(middleware)
 
+        if(this._hasStarted) {
+            this._ioServer.use(middleware.handle)
+        }
+    }
+
+    static async addChannel(channel: SocketChannel){
+        this._channels.push(channel)
+    }
+
+    static async emit(channel: string, ...args){
+        if(!this._hasStarted) return
+
+        this._ioServer.emit(channel, ...args)
     }
 
     static onClientConnection: (socket: Socket) => void = function(socket){}
 }
 
 SocketManager.init
-export { SocketManager }
+export { SocketManager, SocketMiddleware, SocketChannel }
